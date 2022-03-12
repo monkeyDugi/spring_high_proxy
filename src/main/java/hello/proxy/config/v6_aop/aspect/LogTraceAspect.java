33bdaccd1a87395c895e20f9aaadd9f9e59c07df
monkeyDugi <lbd4946@gmail.com>
2022-03-12 09:49:20 +0900

    @Aspect 적용

---
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,39 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    // 어드바이저
+    @Around("execution(* hello.proxy.app..*(..))") // 포인트 컷
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        // 어드바이스
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+            // target 호출
+            Object result = joinPoint.proceed();
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
